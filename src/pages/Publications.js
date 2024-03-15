6ca90c1a36864958d92c445f7e1bec1eb636335c
Astitva Jaiswal <astitvajaiswalreturns@gmail.com>
2024-03-15 13:26:32 +0530

    publications

--- src/pages/Publications.js
+++ src/pages/Publications.js
@@ -7,11 +7,13 @@ export default class Resume extends PureComponent {
         <div className="contentContainer">
           <h3>Publications</h3>
         </div>
-         <object 
-        width="80%"
-        height="80%"
-        data="https://www.researchgate.net/profile/Shawn-Thomas-12/publication/376054169_A_Critical_Analysis_of_Approaches_to_Glaucoma_Detection/links/656ac3603fa26f66f445cd67/A-Critical-Analysis-of-Approaches-to-Glaucoma-Detection.pdf"
-        type="application/pdf">
+        <object
+          width="80%"
+          height="80%"
+          data="/paper.pdf#page=1&zoom=150"
+          type="application/pdf"
+        >
+          {" "}
         </object>
       </div>
     );
